6d17ed4f2dcfee7e699c4a6b20b417f90ff3ddbd
arek <bombarder1118@o2.pl>
2020-05-12 14:59:27 +0200

    changing Ignoreme sh

--- IgnoreMe.sh
+++ IgnoreMe.sh
@@ -16,4 +16,12 @@ g++ -g main.cpp -o prog
 ./prog
 gdb -ex 'quit' -q prog core
 ./sprawdzSie 8
+cd /home/root/exercises/zad3
+cat main_test.cpp
+./tests
+cd /home/root/exercises/zad3_wersja_dla_chetnych
+cat main_test.cpp
+./tests
+exit
+
 
