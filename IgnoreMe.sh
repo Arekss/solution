f1f1ff5d4b65ec914f96566bd3b0ce9ed36816bf
arek <bombarder1118@o2.pl>
2020-05-12 13:45:05 +0200

    dsa

--- IgnoreMe.sh
+++ IgnoreMe.sh
@@ -4,13 +4,13 @@ cd build
 cmake ..
 make
 cp lib/libgtest* /usr/lib
-cd home/root/exercises/zad1
+cd /home/root/exercises/zad1
 ulimit -c unlimited
 g++ -g main.cpp -o prog
 ./prog
 gdb -ex 'quit' -q prog core
 ./SprawdzSie 11
-cd home/root/exercises/zad2
+cd /home/root/exercises/zad2
 ulimit -c unlimited
 g++ -g main.cpp -o prog
 ./prog
